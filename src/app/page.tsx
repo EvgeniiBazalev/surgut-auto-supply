eccbda6504e560ec8a99b449ae26d49f2bf99946
EvgeniiBazalev <bazalevj@gmail.com>
2024-07-22 18:31:20 +0500

    sponsors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroCarousel from "@/components/mainComponents/HeroCarousel";
+import Product from "@/components/mainComponents/Product";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <HeroCarousel></HeroCarousel>
+      <Product></Product>
       <div>start</div>
     </div>
   );
