f3a3e75838be8e1789c7c0dac60b23507a70f5b6
EvgeniiBazalev <bazalevj@gmail.com>
2024-07-22 11:43:59 +0500

    image on hero carousel

--- src/components/mainComponents/HeroCarousel.tsx
+++ src/components/mainComponents/HeroCarousel.tsx
@@ -3,6 +3,7 @@
 import { Carousel } from "flowbite-react";
 
 import React from "react";
+import Image from "next/image";
 
 type Props = {};
 
@@ -10,26 +11,11 @@ const HeroCarousel = (props: Props) => {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel pauseOnHover>
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-          alt="..."
-        />
+        <Image src="/HeroCarousel/1.jpg" alt="..." width={1920} height={400} />
+        <Image src="/HeroCarousel/2.jpeg" alt="..." width={1920} height={400} />
+        <Image src="/HeroCarousel/1.jpg" alt="..." width={1920} height={400} />
+        <Image src="/HeroCarousel/2.jpeg" alt="..." width={1920} height={400} />
+        <Image src="/HeroCarousel/1.jpg" alt="..." width={1920} height={400} />
       </Carousel>
     </div>
   );
